b27af029116d45fa3e011bb4a9cd19a444416e28
xnuray98s <66944976+xnuray98s@users.noreply.github.com>
2021-09-06 07:26:50 +0300

    error handling 10

--- models.py
+++ models.py
@@ -16,7 +16,10 @@ setup_db(app)
 """
 
 
-def setup_db(app, database_path=os.environ["DATABASE_URL"]):
+def setup_db(
+    app,
+    database_path=os.environ["DATABASE_URL"].replace("postgres://", "postgresql://"),
+):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
